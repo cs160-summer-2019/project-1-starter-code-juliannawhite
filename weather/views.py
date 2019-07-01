1a8899589dc83a3c4226080d07e39e50eeb7165a
juliannawhite13 <juliannawhite13@gmail.com>
2019-07-01 06:22:04 -0400

    add all files to github...i think...?

--- weather/views.py
+++ weather/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 
 def forecast(request):
     return render(request, 'weather/forecast.html')
+  
+def search(request):
+    return render(request, 'weather/search.html')
 
 def forecast_alert(request):
     return render(request, 'weather/forecast-alert.html')
